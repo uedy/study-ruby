382548c1a3d15ebec73dfc1f61654468b21b0e86
umpuep <naoki.ueda.612@gmail.com>
2018-03-08 23:10:22 +0900

    3章突入

---
+++ study2-8-1.rb
@@ -0,0 +1,10 @@
+n = 2
+
+if n.zero?
+	puts '0でした'
+elsif n.even?
+	puts '偶数'
+elsif n.odd?
+	puts '偶数でした'
+end
+
---
+++ study3-1-1.rb
@@ -0,0 +1,6 @@
+n = 1
+unless n.zero?
+	puts '0ではありませんでした'
+else
+	puts '0でした'
+end
